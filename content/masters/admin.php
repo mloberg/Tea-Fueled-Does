c77a67763f49fa82c49a5f76566645795b0e58b5
Matthew Loberg <loberg.matt@gmail.com>
2011-06-08 13:32:36 -0500

    added new admin master

---
+++ content/masters/admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title><?php echo $title;?></title>
+	<?php $this->css->echo_stylesheets();?>
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+<?php echo $this->render->flash;?>
+<div id="wrapper">
+<?php echo $content;?>
+
+</div>
+<?php $this->javascript->echo_scripts();?>
+</body>
+</html>
