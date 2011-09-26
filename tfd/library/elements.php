b829c460bcaad8198af71222ef16678a35f71cc3
Matthew Loberg <loberg.matt@gmail.com>
2011-09-26 11:40:06 -0500

    elements class rewrite

--- tfd/library/elements.php
+++ tfd/library/elements.php
@@ -1,10 +1,8 @@
-<?php
+<?php namespace TFD\Library;
 
-	abstract class Elements{}
+	abstract class HTML{
 	
-	abstract class Html extends Elements{
-	
-		static private function text($text,$size,$extra){
+		private static function text($text,$size,$extra){
 			$string = "<{$size}";
 			if($extra !== ''){
 				$string .= " {$extra}";
@@ -13,15 +11,15 @@
 			return $string;
 		}
 		
-		static function p($text,$e=''){return self::text($text,'p',$e);}
-		static function h1($text,$e=''){return self::text($text,'h1',$e);}
-		static function h2($text,$e=''){return self::text($text,'h2',$e);}
-		static function h3($text,$e=''){return self::text($text,'h3',$e);}
-		static function h4($text,$e=''){return self::text($text,'h4',$e);}
-		static function h5($text,$e=''){return self::text($text,'h5',$e);}
-		static function h6($text,$e=''){return self::text($text,'h6',$e);}
+		public static function p($text,$e=''){return self::text($text,'p',$e);}
+		public static function h1($text,$e=''){return self::text($text,'h1',$e);}
+		public static function h2($text,$e=''){return self::text($text,'h2',$e);}
+		public static function h3($text,$e=''){return self::text($text,'h3',$e);}
+		public static function h4($text,$e=''){return self::text($text,'h4',$e);}
+		public static function h5($text,$e=''){return self::text($text,'h5',$e);}
+		public static function h6($text,$e=''){return self::text($text,'h6',$e);}
 		
-		static private function html_list($type,$list_items,$extra){
+		private static function html_list($type,$list_items,$extra){
 			$string = "<{$type}";
 			if($extra !== ''){
 				$string .= " {$extra}";
@@ -34,20 +32,20 @@
 			return $string;
 		}
 		
-		static function br(){return "<br />\n";}
+		public static function br(){return "<br />\n";}
 		
-		static function ul($li,$e=''){return self::html_list('ul',$li,$e);}
-		static function ol($li,$e=''){return self::html_list('ol',$li,$e);}
+		public static function ul($li,$e=''){return self::html_list('ul',$li,$e);}
+		public static function ol($li,$e=''){return self::html_list('ol',$li,$e);}
 		
-		static function img(){
+		public static function img(){
 		
 		}
 	
 	}
 	
-	abstract class Form extends Elements{
+	abstract class Form{
 	
-		static function open($action,$method='post',$e=''){
+		public static function open($action,$method='post',$e=''){
 			if(is_array($e)){
 				foreach($e as $k => $v){
 					$extra .= " {$k}=\"{$v}\"";
@@ -58,7 +56,7 @@
 			return "<form action=\"{$action}\" method=\"{$method}\"{$extra}>\n";
 		}
 		
-		static function open_upload($action,$extra=''){
+		public static function open_upload($action,$extra=''){
 			if($extra !== ''){
 				$extra = $extra . 'enctype="multipart/form-data"';
 			}else{
@@ -67,9 +65,9 @@
 			return self::open($action,'post',$extra);
 		}
 		
-		static function label($for,$text){return "<label for=\"{$for}\">{$text}</label>";}
+		public static function label($for,$text){return "<label for=\"{$for}\">{$text}</label>";}
 		
-		static function form_input($type,$name,$value,$extra){
+		private static function form_input($type,$name,$value,$extra){
 			if($_POST[$name] && $_POST[$name] !== 'submit'){
 				$value = $_POST[$name];
 			}elseif($_GET[$name] && $_GET[$name] !== 'submit'){
@@ -90,11 +88,11 @@
 			return $string;
 		}
 		
-		static function input($name,$value='',$opts=''){return self::form_input('text',$name,$value,$opts);}
-		static function password($name,$value='',$opts=''){return self::form_input('password',$name,$value,$opts);}
-		static function file_upload($name,$opts=''){return self::form_input('file',$name,'',$opts);}
-		static function submit($value,$opts=''){return self::form_input("submit","submit",$value,$opts);}
-		static function textarea($name,$value='',$opts=''){
+		public static function input($name,$value='',$opts=''){return self::form_input('text',$name,$value,$opts);}
+		public static function password($name,$value='',$opts=''){return self::form_input('password',$name,$value,$opts);}
+		public static function file_upload($name,$opts=''){return self::form_input('file',$name,'',$opts);}
+		public static function submit($value,$opts=''){return self::form_input("submit","submit",$value,$opts);}
+		public static function textarea($name,$value='',$opts=''){
 			$string = "<textarea name=\"{$name}\"";
 			if(is_array($opts)){
 				foreach($opts as $k => $v){
@@ -112,7 +110,7 @@
 			return $string;
 		}
 		
-		static function dropdown($name,$values,$selected='',$opts=''){
+		public static function dropdown($name,$values,$selected='',$opts=''){
 			$string = "<select name=\"{$name}\"";
 			if(is_array($opts)){
 				foreach($opts as $k => $v){
@@ -133,7 +131,7 @@
 			return $string;
 		}
 		
-		static function multi($type,$name,$values,$extra){
+		public static function multi($type,$name,$values,$extra){
 			$template = "<input type=\"{$type}\" name=\"{$name}\"";
 			foreach($values as $k => $v){
 				if($v !== '') $v = ' '.$v;
@@ -150,9 +148,9 @@
 			return $string;
 		}
 		
-		static function radio($name,$values,$opts=''){return self::multi('radio',$name,$values,$opts);}
-		static function checkbox($name,$values,$opts=''){return self::multi("checkbox",$name,$values,$opts);}
+		public static function radio($name,$values,$opts=''){return self::multi('radio',$name,$values,$opts);}
+		public static function checkbox($name,$values,$opts=''){return self::multi("checkbox",$name,$values,$opts);}
 		
-		static function close(){return "</form>\n";}
+		public static function close(){return "</form>\n";}
 	
 	}
