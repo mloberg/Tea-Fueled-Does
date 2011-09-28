f8637028c59b2cd6d72a624e819577fb7a81a1ea
Matthew Loberg <loberg.matt@gmail.com>
2011-09-28 08:35:05 -0500

    request class cleanup. Ajax class rewrite

--- tfd/core/request.php
+++ tfd/core/request.php
@@ -1,6 +1,7 @@
 <?php namespace TFD\Core;
 
 	use \TFD\Admin;
+	use \Content\Ajax;
 	
 	class Request{
 	
@@ -25,48 +26,43 @@
 		}
 		
 		public static function run(){
-			if(MAINTENANCE_MODE){
-				// Render the maintenance page
+			if(self::is_maintenance()){
 				
-			}elseif(preg_match('/^'.preg_quote(AJAX_PATH).'\/(.*)$/', self::$request)){
-				// ajax request
-				
-			}elseif(preg_match('/^'.preg_quote(LOGIN_PATH).'\/?$/', self::$request)){
-				// login page
+			}elseif(($call = self::is_ajax_request()) !== false){
+				return (string) new Ajax($call);
+			}elseif(self::is_login()){
 				return Admin::login();
-			}elseif(preg_match('/^'.preg_quote(ADMIN_PATH).'(\/?(.*))$/', self::$request)){
-				// admin dahboard
+			}elseif(self::is_admin()){
 				return Admin::dashboard();
-			}elseif(ADD_USER && self::$request === 'index' && array_key_exists('add_user', $_GET) && $_GET['username'] && $_GET['password']){
-				// add a user
-			}
-			
-			return false;
-/*
-			if(MAINTENANCE_MODE){
-				// render new maintenance page
-				
-			}elseif(preg_match('/'.preg_quote(AJAX_PATH).'\/(.*)$/', self::$request)){
-				// ajax request
-				
-				// old ajax stuff
-				if(empty($_GET['ajax'])){
-					$_GET['ajax'] = preg_replace('/^(.*)'.MAGIC_AJAX_PATH.'\//', '', $this->request());
-				}
-				return $this->ajax->call();
-			}elseif(preg_match('/^'.preg_quote(LOGIN_PATH).'/', self::$request)){
-				// login page
-				return Admin::login();
-			}elseif(preg_match('/^'.preg_quote(ADMIN_PATH).'/', self::$request) && Admin::loggedin()){
-				// dashboard page
-				return Admin::dashboard();
-			}elseif(ADD_USER && self::$request === 'index' && isset($_GET['add_user']) && !empty($_GET['username']) && !empty($_GET['password'])){
-				// add a user
+			}elseif(self::is_add_user()){
 				
 			}
 			
 			return false;
-*/
+		}
+		
+		private static function is_maintenance(){
+			return (defined('MAINTENANCE_MODE') && MAINTENANCE_MODE === true) ? true : false;
+		}
+		
+		private static function is_ajax_request(){
+			if(defined('AJAX_PATH') && preg_match('/^'.preg_quote(AJAX_PATH).'\/?(.*)?$/', self::$request, $match) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+				if(empty($match[1]) && defined('AJAX_REQUEST_PARAM') && isset($_REQUEST[AJAX_REQUEST_PARAM])) return $_REQUEST[AJAX_REQUEST_PARAM];
+				return $match[1];
+			}
+			return false;
+		}
+		
+		private static function is_login(){
+			return (defined('LOGIN_PATH') && preg_match('/^'.preg_quote(LOGIN_PATH).'\/?$/', self::$request)) ? true : false;
+		}
+		
+		private static function is_admin(){
+			return (defined('ADMIN_PATH') && preg_match('/^'.preg_quote(ADMIN_PATH).'\/?(.*)?$/', self::$request)) ? true : false;
+		}
+		
+		private static function is_add_user(){
+			return (defined(ADD_USER) && self::$request === 'index' && isset($_GET['add_user']) && !empty($_GET['username']) && $_GET['password']) ? true : false;
 		}
 	
 	}
