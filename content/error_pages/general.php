09db15aa489ea1d079a0799ab5d45ace457f991b
Matthew Loberg <loberg.matt@gmail.com>
2011-09-28 13:24:45 -0500

    ErrorPage Render class.

---
+++ content/error_pages/general.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Error</title>
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<p>ran into an error.</p>
+</body>
+</html>
