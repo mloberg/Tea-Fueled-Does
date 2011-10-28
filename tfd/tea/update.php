3634e0362ba45bd23559b6cb1c8d5edc0da9cc0f
Matthew Loberg <loberg.matt@gmail.com>
2011-10-28 09:14:42 -0500

    Tea update wip.

--- tfd/tea/update.php
+++ tfd/tea/update.php
@@ -7,7 +7,24 @@
 		);
 		
 		public static function action($arg){
-			
+			if(empty($arg)) self::help();
+		}
+		
+		public static function help(){
+			echo <<<MAN
+Update TFD.
+
+	Usage: tea update <args>
+
+Arguments:
+
+	-h, --help    This page
+
+TFD Homepage: http://teafueleddoes.com/
+Tea Homepage: http://teafueleddoes.com/v2/tea
+
+MAN;
+			exit(0);
 		}
 	
 	}
