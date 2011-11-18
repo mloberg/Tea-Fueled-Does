0d7e8c7406df2b8a6b96cf1b4dac15f2dcaf5e9c
Matthew Loberg <loberg.matt@gmail.com>
2011-11-18 12:40:50 -0600

    added Tea update command

---
+++ tfd/tea/update.php
@@ -0,0 +1,49 @@
+<?php namespace TFD\Tea;
+
+	use TFD\File;
+	
+	class Update{
+	
+		public static function action($arg){
+			$latest = file_get_contents('http://get.teafueleddoes.com/update.php?latest');
+			$current = File::get(BASE_DIR.'.tfdrevision');
+			if($latest == $current){
+				echo "You are up to date!\n";
+				exit(0);
+			}elseif(Tea::yes_no('Would you like to update TFD?')){
+				$update = json_decode(self::post_request('changes', array('from' => $current)), true);
+				if(is_array($update['delete'])){
+					foreach($update['delete'] as $delete){
+						@unlink($delete);
+						if(file_exists($delete)) echo "\nCould not delete {$delete}!";
+					}
+				}
+				if(is_array($update['update'])){
+					foreach($update['update'] as $file => $content){
+						$checksum = md5_file($file);
+						@unlink($file);
+						File::put($file, base64_decode($content));
+						if(md5_file($file) == $checksum) echo "\nCould not update {$file}!";
+					}
+				}
+				File::put(BASE_DIR.'.tfdrevision', $update['sha']);
+				echo $update['message'];
+				echo "TFD updated!\n";
+			}
+		}
+		
+		private static function post_request($method, $params){
+			$post = array(
+				'time' => md5(time()),
+				'method' => $method
+			) + $params;
+			$ch = curl_init('http://get.teafueleddoes.com/update.php');
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$resp = curl_exec($ch);
+			curl_close($ch);
+			return $resp;
+		}
+	
+	}
