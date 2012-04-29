162470d7627004642c228fb039102d3aff212826
Matthew Loberg <loberg.matt@gmail.com>
2012-04-28 19:13:29 -0700

    Added comments to benchmark class

--- tfd/benchmark.php
+++ tfd/benchmark.php
@@ -1,40 +1,80 @@
 <?php namespace TFD;
 
-	class Benchmark{
+	class Benchmark {
 	
 		private static $marks = array();
 		private static $memory = array();
-		
-		public static function start($name){
-			self::$marks[$name] = microtime(true);
+
+		/**
+		 * Start a timer.
+		 *
+		 * @param string $name Timer name
+		 */
+
+		public static function start($name) {
+			static::$marks[$name] = microtime(true);
 		}
-		
-		public function check($name, $decimal = 4){
-			if(array_key_exists($name, self::$marks)){
-				return round(microtime(true) - self::$marks[$name], $decimal);
+
+		/**
+		 * Check a timer.
+		 * 
+		 * @param string $name Timer name
+		 * @param integer $decimal Decimal places to return
+		 * @return float The time since the timer was started
+		 */
+
+		public function check($name, $decimal = 4) {
+			if (array_key_exists($name, self::$marks)) {
+				return round(microtime(true) - static::$marks[$name], $decimal);
 			}
 			return 0.0;
 		}
-		
-		public function memory($name){
-			self::$memory[$name] = memory_get_usage();
+
+		/**
+		 * Start memory usage benchmark.
+		 * 
+		 * @param string $name Benchmark name
+		 */
+
+		public function memory($name) {
+			static::$memory[$name] = memory_get_usage();
 		}
-		
-		public function used_memory($name, $decimal = 2){
-			if(array_key_exists($name, self::$memory)){
-				return round((memory_get_usage() - self::$memory[$name]) / 1024 / 1024, $decimal);
+
+		/**
+		 * Get the memory usage since benchmark start.
+		 * 
+		 * @param string $name Benchmark name
+		 * @param integer $decimals Decimal places to return
+		 */
+
+		public function used_memory($name, $decimal = 2) {
+			if (array_key_exists($name, self::$memory)) {
+				return round((memory_get_usage() - ststic::$memory[$name]) / 1024 / 1024, $decimal);
 			}
 			return 0.0;
 		}
-		
-		public function current_memory($decimal = 2){
+
+		/**
+		 * Get current memory usage.
+		 * 
+		 * @param integer $decimal Decimal places to return
+		 */
+
+		public function current_memory($decimal = 2) {
 			// return memory usage in mb
 			return number_format(memory_get_usage() / 1024 / 1024, $decimal);
 		}
-		
-		public function peak_memory($decimal = 2){
+
+		/**
+		 * Get peak memory usage of session
+		 *
+		 * @param integer $decimal Decimal places to return
+		 */
+
+		public function peak_memory($decimal = 2) {
 			// return memory in mb
 			return number_format(memory_get_peak_usage() / 1024 / 1024, $decimal);
 		}
 	
 	}
+
