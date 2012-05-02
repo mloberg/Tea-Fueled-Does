819dfd489af4728c7e9b0a372bd7a2055f54fbaa
Matthew Loberg <loberg.matt@gmail.com>
2012-05-02 12:10:02 -0500

    Added comments to Model class.

--- tfd/model.php
+++ tfd/model.php
@@ -4,17 +4,27 @@
 	 * Return an instance of a user model
 	 */
 	
-	class Model{
+	class Model {
 	
-		public static function __callStatic($name, $arguments){
+		/**
+		 * Allow for static calls to return a new instance of a model.
+		 *
+		 * Example: Model::model_name();
+		 */
+
+		public static function __callStatic($name, $arguments) {
 			array_unshift($arguments, $name);
 			return call_user_func_array('self::make', $arguments);
 		}
+
+		/**
+		 * Return a new instance of a model.
+		 */
 		
-		public static function make(){
+		public static function make() {
 			$args = func_get_args();
 			$name = array_shift($args);
-			if(empty($name)){
+			if (empty($name)){
 				throw new \Exception('Missing name argument for model::make');
 			}
 			$model = new \ReflectionClass('\Content\Models\\'.$name);
