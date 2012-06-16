cde4e8aa7cfa6a4449dfc4cc07251720749fbdbd
Matthew Loberg <loberg.matt@gmail.com>
2012-06-16 12:45:44 -0500

    Starting cache class changes.
    Modified the interface for cache classes.
    Rewrote the APC cache class.
    Added tests for the APC cache class.

--- Core/Cache/APC.php
+++ Core/Cache/APC.php
@@ -2,33 +2,94 @@
 
 	use TFD\Core\Config;
 	
-	class APC implements Driver{
+	class APC implements Driver {
 	
-		public function has($key){
-			return (!is_null(this->get(Config::get('cache.key').$key)));
+		/**
+		 * Check for the existance of a key.
+		 *
+		 * @param string $key Cache key
+		 * @return boolean True if key exists
+		 */
+		
+		public function has($key) {
+			return apc_exists($key);
 		}
 		
-		public function get($key){
-			return (($cache = apc_fetch(Config::get('cache.key').$key)) !== false) ? $cache : null;
+		/**
+		 * Get the value of a key.
+		 *
+		 * @param string $key Cache key
+		 * @return mixed Cache value if exists, otherwise null
+		 */
+		
+		public function get($key) {
+			return (($cache = apc_fetch($key)) !== false) ? $cache : null;
 		}
 		
-		public function set($key, $value, $time){
-			apc_store(Config::get('cache.key').$key, $value, $time);
+		/**
+		 * Set a single cache item.
+		 *
+		 * @param string $key Cache key
+		 * @param mixed $value Cached value
+		 * @param integer $time Time to live in seconds
+		 * @return boolean True on success
+		 */
+		
+		public function set($key, $value, $time = 0) {
+			return apc_store($key, $value, $time);
+		}
+
+		/**
+		 * Store an array of cache items.
+		 *
+		 * @param array $values A key value array of cache items
+		 * @param integer $time Time to live in seconds
+		 */
+
+		public function store($values, $time = 0) {
+			return apc_store($values, null, $time);
 		}
 		
-		public function delete($key){
-			return apc_delete(Config::get('cache.key').$key);
+		/**
+		 * Delete a cache item.
+		 *
+		 * @param string $key Cache key
+		 * @return True on success
+		 */
+		
+		public function delete($key) {
+			return apc_delete($key);
 		}
 		
-		public function flush(){
+		/**
+		 * Delete all cache items.
+		 */
+		
+		public function flush() {
 			return apc_clear_cache('user'); // clear only user cache
 		}
+
+		/**
+		 * Increase a stored number.
+		 *
+		 * @param string $key Cache key
+		 * @param integer $count Increase by
+		 * @return integer Key's value
+		 */
 		
-		public function increase($key, $count = 1){
+		public function increase($key, $count = 1) {
 			return apc_inc($key, $count);
 		}
+
+		/**
+		 * Decrease a stored number.
+		 *
+		 * @param string $key Cache key
+		 * @param integer $count Decrease by
+		 * @return integer Key's value
+		 */
 		
-		public function decrease($key, $count = 1){
+		public function decrease($key, $count = 1) {
 			return apc_dec($key, $count);
 		}
 	
--- Core/Cache/Driver.php
+++ Core/Cache/Driver.php
@@ -1,33 +1,56 @@
 <?php namespace TFD\Core\Cache;
 
-	interface Driver{
+	interface Driver {
 	
 		/**
-		 * Check for the existance of a key
+		 * Check for the existance of a key.
+		 *
+		 * @param string $key Cache key
+		 * @return boolean True if key exists
 		 */
 		
 		public function has($key);
 		
 		/**
-		 * Get the value of a key
+		 * Get the value of a key.
+		 *
+		 * @param string $key Cache key
+		 * @return mixed Cache value if exists, otherwise null
 		 */
 		
 		public function get($key);
 		
 		/**
-		 * Set a key
+		 * Set a single cache item.
+		 *
+		 * @param string $key Cache key
+		 * @param mixed $value Cached value
+		 * @param integer $time Time to live in seconds
+		 * @return boolean True on success
 		 */
 		
-		public function set($key, $value, $time);
+		public function set($key, $value, $time = 0);
+
+		/**
+		 * Store an array of cache items.
+		 *
+		 * @param array $values A key value array of cache items
+		 * @param integer $time Time to live in seconds
+		 */
+
+		public function store($values, $time = 0);
 		
 		/**
-		 * Delete a key
+		 * Delete a cache item.
+		 *
+		 * @param string $key Cache key
+		 * @return True on success
 		 */
 		
 		public function delete($key);
 		
 		/**
-		 * Delete all keys
+		 * Delete all cache items.
 		 */
 		
 		public function flush();
